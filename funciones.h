8efc54521381092aa09fced7d12d595e44b7b16a
Samuel-119052 <samusmusic18@gmail.com>
2025-06-29 22:28:48 -0500

    Añadiendo estructura y funcion eliminar contactos

--- funciones.h
+++ funciones.h
@@ -1,6 +1,20 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
-
+#include<string>
+#include<iostream>
+using namespace std;
+const int Ctc = 100;
+struct contactoEmail{
+	string Nom;
+	string Sx;
+	int Eda;
+	string Tel;
+	string Mail;
+	string Nac;
+};
+contactoEmail Ag[Ctc];
+int Tctc = 0;
 void Agregarcontacto();
+void Eliminarcontacto();
 
 #endif
